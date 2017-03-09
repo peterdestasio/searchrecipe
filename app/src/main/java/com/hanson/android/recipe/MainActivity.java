ad16aec57c7587f2739f82aef100c322cbe03636
shyjoo <shyjoo@live.com>
2017-03-09 16:09:18 -0500

    Create Slide menu and entrance page

--- app/src/main/java/com/hanson/android/recipe/MainActivity.java
+++ app/src/main/java/com/hanson/android/recipe/MainActivity.java
@@ -3,6 +3,7 @@ package com.hanson.android.recipe;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +14,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.hanson.android.recipe.dummy.DummyContent;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, CategoryListFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +30,29 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Check search recipes menu on the slide menu
+                NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+                navigationView.setCheckedItem(R.id.nav_search);
+                //connect to search recipes page
+                FragmentManager manager = getSupportFragmentManager();
+                SearchFragment searchFragment = new SearchFragment();
+                manager.beginTransaction().replace(R.id.root_layout, searchFragment, searchFragment.getTag()).addToBackStack(null).commit();
             }
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.root_layout, homeFragment, homeFragment.getTag()).commit();
     }
 
     @Override
@@ -52,50 +65,67 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        FragmentManager manager = getSupportFragmentManager();
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_home)
+        {
+            //connect to home page
+            HomeFragment homeFragment = new HomeFragment();
+            manager.beginTransaction().replace(R.id.root_layout, homeFragment, homeFragment.getTag()).addToBackStack(null).commit();
 
-        } else if (id == R.id.nav_manage) {
+        }
+        else if (id == R.id.nav_login)
+        {
+            //connect to login page
+            LoginFragment loginFragment = new LoginFragment();
+            manager.beginTransaction().replace(R.id.root_layout, loginFragment, loginFragment.getTag()).addToBackStack(null).commit();
 
-        } else if (id == R.id.nav_share) {
+        }
+        else if (id == R.id.nav_category)
+        {
+            //connect to category page
+            CategoryListFragment categoryListFragment = new CategoryListFragment();
+            manager.beginTransaction().replace(R.id.root_layout, categoryListFragment, categoryListFragment.getTag()).addToBackStack(null).commit();
 
-        } else if (id == R.id.nav_send) {
+        }
+        else if (id == R.id.nav_search)
+        {
+            //connect to search recipes page
+            SearchFragment searchFragment = new SearchFragment();
+            manager.beginTransaction().replace(R.id.root_layout, searchFragment, searchFragment.getTag()).addToBackStack(null).commit();
+        }
+        else if (id == R.id.nav_add)
+        {
+            //connect to add recipe page
+            AddRecipeFragment addRecipeFragment = new AddRecipeFragment();
+            manager.beginTransaction().replace(R.id.root_layout, addRecipeFragment, addRecipeFragment.getTag()).addToBackStack(null).commit();
+
+        }
+        else if (id == R.id.nav_info)
+        {
+            //connect to information page
+            InformationFragment informationFragment = new InformationFragment();
+            manager.beginTransaction().replace(R.id.root_layout, informationFragment, informationFragment.getTag()).addToBackStack(null).commit();
+
+        }
+        else if (id == R.id.nav_logout)
+        {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
