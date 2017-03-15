44e1eb6ddb8afb271cb63cf80ddd40d4ab8d18d9
peterdestasio <peterdestasio@gmail.com>
2017-03-14 21:12:37 -0400

    Added Collection Picker Libraries for add String in a Picker, Created the UI of Search Fragment

--- app/src/main/java/com/hanson/android/recipe/SearchFragment.java
+++ app/src/main/java/com/hanson/android/recipe/SearchFragment.java
@@ -1,17 +1,37 @@
 package com.hanson.android.recipe;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.anton46.collectionitempicker.CollectionPicker;
+import com.anton46.collectionitempicker.Item;
+import com.anton46.collectionitempicker.OnItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements View.OnClickListener {
+
+    EditText editText;
+    Button addButton;
+    Button buttonSearch;
+
+    List<Item> items = new ArrayList<>();
+    CollectionPicker picker;
+
 
 
     public SearchFragment() {
@@ -22,8 +42,47 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        editText = (EditText)view.findViewById(R.id.typeIngredient);
+        addButton = (Button)view.findViewById(R.id.addIngredient);
+        picker = (CollectionPicker)view.findViewById(R.id.collection_item_picker);
+        buttonSearch = (Button)view.findViewById(R.id.buttonSearch);
+
+        addButton.setOnClickListener(this);
+        buttonSearch.setOnClickListener(this);
+
+        return  view;
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==addButton.getId()){
+            items.add(new Item("item1", editText.getText().toString()));
+            picker.setItems(items);
+            picker.drawItemView();
+
+
+            picker.setOnItemClickListener(new OnItemClickListener() {
+                @Override
+                public void onClick(Item item, int position) {
+                    //to do
+                }
+            });
+
+        }
+
+        if(v.getId()==buttonSearch.getId()){
+
+            //to do
+
+        }
+
+    }
+
+
+
 
 }
